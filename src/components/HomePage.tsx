dd35d5797a0bd44296084b9df617066d5b35f688
Luis Aleixo <luis.aleixo@cern.ch>
2023-09-29 16:54:27 +0200

    First commit

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,17 @@
+const HomePage = () => {
+    return (
+        <div>
+            <h3>Hockey for Benim Starter Page</h3>
+
+            <p>Hockey for Benim</p>
+            <p>📍Projecto “Hockey For Benim” 🇧🇯</p>
+            <br/>
+            <p>📣 Vamos ajudar o desenvolvimento do hóquei em patins em Cotonou em Benim, África. 🏒</p>
+            <br/>
+            <p>👉 Segue esta aventura <a href="gofund.me/7789f64c">aqui</a> 👈</p>
+        </div>
+    )
+    
+}
+
+export default HomePage;
