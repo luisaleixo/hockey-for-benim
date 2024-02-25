53b299d1e66d51d88ec5152b3b570990507da2e0
Luis Aleixo <luis.aleixo@cern.ch>
2024-02-25 19:37:57 +0100

    added connection to server

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -12,11 +12,53 @@ import { Element } from "react-scroll";
 import FormTextField from "../landing/FormTextField";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
+import { useState } from "react";
 
 const Contacts: React.FC<{
   theme: Theme;
   name: string;
 }> = (props) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    message: "",
+  });
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const response = await fetch("https://hockey-for-benim-mail.vercel.app/api/sendMail", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-KEY": "jqyk zwfr obuo eczn",
+      },
+      body: JSON.stringify({
+        to: "luis.aleixo97@hotmail.com",
+        from: "general.hockeyforbenim@gmail.com",
+        subject: "New Message from Contact Form",
+        html: `<h1>Name:</h1> ${formData.name}<br/>`
+      }),
+    });
+
+    if (response.ok) {
+      // Request was successful
+      console.log("Email sent successfully");
+    } else {
+      // Request failed
+      console.error("Error sending email");
+    }
+  };
+
   const topContainer = {
     height: "10vh",
     width: "100vw",
@@ -24,7 +66,7 @@ const Contacts: React.FC<{
     flexDirection: "row",
   };
   const container = {
-    height: {lg: "95vh", xs: "auto"},
+    height: { lg: "95vh", xs: "auto" },
     display: "flex",
     flexDirection: { lg: "row", xs: "column" },
   };
@@ -63,7 +105,11 @@ const Contacts: React.FC<{
             }}
           ></Box>
           <Box
-            sx={{ display: "flex", flex: {lg:"1 0 50%", xs: "1 0 10%"}, background: "white" }}
+            sx={{
+              display: "flex",
+              flex: { lg: "1 0 50%", xs: "1 0 10%" },
+              background: "white",
+            }}
           ></Box>
         </Box>
         <Box sx={container}>
@@ -131,18 +177,38 @@ const Contacts: React.FC<{
                 </Typography>
                 <br />
                 <FormControl fullWidth>
-                  <FormTextField label="Nome" multiline={false} />
-                  <br />
-                  <FormTextField label="Email" multiline={false} />
-                  <br />
-                  <FormTextField
-                    label="Contacto Telefonico"
-                    multiline={false}
+                  <input
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    placeholder="Nome"
                   />
                   <br />
-                  <FormTextField label="Mensagem" multiline={true} />
+                  <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    placeholder="Email"
+                  />
                   <br />
-                  <Button variant="contained" color="error" size="large">
+                  <input
+                    type="tel"
+                    name="phoneNumber"
+                    value={formData.phoneNumber}
+                    onChange={handleChange}
+                    placeholder="Contacto Telefonico"
+                  />
+                  <br />
+                  <textarea
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                    placeholder="Mensagem"
+                  />
+                  <br />
+                  <Button variant="contained" color="error" size="large" onClick={handleSubmit}>
                     <strong>Enviar</strong>
                   </Button>
                 </FormControl>
@@ -162,7 +228,11 @@ const Contacts: React.FC<{
             }}
           ></Box>
           <Box
-            sx={{ display: "flex", flex: {lg: "1 0 50%", xs: "1 0 10%"}, background: "white" }}
+            sx={{
+              display: "flex",
+              flex: { lg: "1 0 50%", xs: "1 0 10%" },
+              background: "white",
+            }}
           ></Box>
         </Box>
       </Element>
